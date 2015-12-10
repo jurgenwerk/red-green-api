7a406114d78ef43d68bfe62ac52001ecfd18542c
Matic Jurglic <matic@jurglic.si>
2015-12-11 00:16:11 +0100

    setup cors

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module RedGreenApi2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before ActionDispatch::ShowExceptions, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
