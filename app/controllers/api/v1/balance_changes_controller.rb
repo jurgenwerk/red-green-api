2b7c7cd57c528dda1518640d3e5316e1d158661b
Matic Jurglic <matic@jurglic.si>
2016-01-22 19:29:09 +0100

    add create action and amount validation

--- app/controllers/api/v1/balance_changes_controller.rb
+++ app/controllers/api/v1/balance_changes_controller.rb
@@ -6,6 +6,20 @@ module Api
       def index
         render json: current_user.balance_changes
       end
+
+      def create
+        balance_change = current_user.balance_changes.create(balance_change_params)
+        if balance_change.valid?
+          render json: balance_change, status: :created
+        else
+          render json: ErrorSerializer.serialize(balance_change.errors), status: :unprocessable_entity
+        end
+      end
+
+      private
+      def balance_change_params
+        params.require(:data).require(:attributes).permit(:value, :change_type)
+      end
     end
   end
 end
