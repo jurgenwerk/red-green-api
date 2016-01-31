a2a7d4743873b12a9a55f6a89d5f56f77d9a9ecc
Matic Jurglic <matic@jurglic.si>
2016-01-31 16:01:30 +0100

    cors fix

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,12 @@ module RedGreenApi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before ActionDispatch::ShowExceptions, Rack::Cors do
+    config.middleware.insert 0, Rack::Cors do
       allow do
         origins '*'
         resource '*',
           :headers => :any,
-          :methods => [:get, :post, :put, :delete, :options]
+          :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
   end
