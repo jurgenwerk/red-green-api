f06fb322e70c1595f421bab52ee949386ceb961f
Matic Jurglic <matic@jurglic.si>
2016-01-31 16:32:24 +0100

    value must be positive

--- app/models/balance_change.rb
+++ app/models/balance_change.rb
@@ -3,5 +3,5 @@ class BalanceChange < ActiveRecord::Base
   has_many :categories, through: :balance_change_categories
   enum change_type: [:income, :expense]
   validates :value, :entry_date, presence: true
-  validates_numericality_of :value
+  validates_numericality_of :value, greater_than: 0
 end
