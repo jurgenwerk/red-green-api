c3a54f3c5b3a9a52619396ebbeaa982b3e35ff21
Matic Jurglic <matic@jurglic.si>
2016-03-26 15:23:50 +0100

    email should be present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
   has_many :balance_changes
 end
